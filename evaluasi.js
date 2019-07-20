19838bbcfc2b1340d81e6f1587bf8042280f00f5
Hudzaifah1610 <52727148+Hudzaifah1610@users.noreply.github.com>
2019-07-20 21:17:52 +0700

    Add files via upload

---
+++ evaluasi.js
@@ -0,0 +1,51 @@
+                            //BISMILLAHIRRAHMANIRRAHIM
+                    //   (TUGAS/UJIAN/EVALUASI MINGGUAN)  \\
+
+
+// SOAL PERTAMA-----
+
+var n = 4;
+
+for(i=0; i<=n; i++){
+    for(h=0; h<=n; h++){
+        if(i == h){
+            document.write(" + ")
+        }else{
+            document.write(" + ")
+        }
+    }
+    document.write('<br>')
+}
+document.write('<br>')
+// --------------------------------------------------------------
+var n = 6;
+
+for(i=0; i<=n; i++){
+    for(h=0; h<=n; h++){
+        if(i % 0 || h == 3 || i == 3){
+            document.write("+")
+        }else{
+            document.write(" - ")
+        }
+    }
+    document.write('<br>')
+}
+document.write('<br>')
+
+
+// SOAL KEDUA-------
+
+var n = 20;
+angkaGenap = []
+angkaGanjil= []
+
+angkaGenap.push('2,4,6,8,10')
+document.write(angkaGenap.toString())
+document.write('<br>')
+// ------------------------------------------------------------------------------
+angkaGanjil.push('1,3,5,7,9')
+document.write(angkaGanjil.toString())
+document.write('<br>')
+
+
+                    //    --- SEMOGA SUKSES ---     \\
